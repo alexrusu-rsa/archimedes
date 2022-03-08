beff4c9e983c8c9d776bfcaee14b3fc6db6c719f
calinc2010 <33434438+calinc2010@users.noreply.github.com>
2022-03-08 12:04:08 +0200

    heroku deploy again

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const path = require('path');
+const path = require("path");
+const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/dist/archimedes-frontend'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/archimedes-frontend/index.html'));});
+app.use(express.static(__dirname + '/'));
+app.get('/*', function(req,res){
+res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
