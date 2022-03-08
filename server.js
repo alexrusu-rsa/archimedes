872832f7966ec5b4ab220a354d5473c090b3a27f
calinc2010 <33434438+calinc2010@users.noreply.github.com>
2022-03-08 12:13:48 +0200

    heroku fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
+res.sendFile(path.join(__dirname, 'archimedes-frontend', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
