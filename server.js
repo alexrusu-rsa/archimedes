0024799a98455ced1882d8899fd16c99f1f82bef
calinc2010 <33434438+calinc2010@users.noreply.github.com>
2022-03-08 12:28:16 +0200

    heroku dist path

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/archimedes-frontend'));
+app.use(express.static(__dirname + '/dist/archimedes-frontend'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'archimedes-frontend', 'index.html'))
+res.sendFile(path.join(__dirname, '/dist/archimedes-frontend', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
