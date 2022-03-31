ccabf30bfe063f9bdd17c0ca4fbfdcec4fd86b47
calinc2010 <33434438+calinc2010@users.noreply.github.com>
2022-03-31 16:30:13 +0300

    heroku setup for testing

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -13,7 +13,8 @@ import { NotificationService } from './notification.service';
   providedIn: 'root',
 })
 export class CustomerService {
-  private customersUrl = 'http://localhost:3000/customer';
+  private customersUrl =
+    'https://archimedes-backend-dev.herokuapp.com/customer';
   httpOptions = {
     header: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
@@ -54,7 +55,7 @@ export class CustomerService {
       .post<RequestWrapper>(this.customersUrl, customer)
       .pipe(catchError(this.handleError<RequestWrapper>('addCustomer')));
   }
-  
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (err: HttpErrorResponse): Observable<T> => {
       console.error(err);
