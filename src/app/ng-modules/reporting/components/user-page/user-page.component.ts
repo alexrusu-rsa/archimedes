9502058d11a4afb7db31da928f586d0a757625e0
calinc2010 <33434438+calinc2010@users.noreply.github.com>
2022-04-14 17:32:16 +0300

    fix

--- src/app/ng-modules/reporting/components/user-page/user-page.component.ts
+++ src/app/ng-modules/reporting/components/user-page/user-page.component.ts
@@ -64,7 +64,6 @@ export class UserPageComponent implements OnInit, OnDestroy {
     });
 
     dialogRef.afterClosed().subscribe((newUser: User) => {
-      console.log(newUser);
       if (newUser) this.allUsers.push(newUser);
     });
   }
