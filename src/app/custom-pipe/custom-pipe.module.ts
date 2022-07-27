05efb14687dd60fba82dbdebab186ec1dbb1f59e
Coroian Calin Andrei <coro@Alexs-MBP.Dlink>
2022-07-27 17:11:48 +0300

    add customerName: Project chips on activities

--- src/app/custom-pipe/custom-pipe.module.ts
+++ src/app/custom-pipe/custom-pipe.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectidPipe } from '../pipes/projectid.pipe';
 import { EmployeeidPipe } from '../pipes/employeeid.pipe';
+import { CustomerfromprojectidPipe } from '../pipes/customerfromprojectid.pipe';
 
 @NgModule({
-  declarations: [ProjectidPipe, EmployeeidPipe],
+  declarations: [ProjectidPipe, EmployeeidPipe, CustomerfromprojectidPipe],
   imports: [CommonModule],
-  exports: [ProjectidPipe, EmployeeidPipe],
+  exports: [ProjectidPipe, EmployeeidPipe, CustomerfromprojectidPipe],
 })
 export class CustomPipeModule {}
