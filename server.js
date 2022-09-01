9b7ea5935d28d117087df8098e948f32b008102b
Coroian Calin Andrei <coro@Alexs-MBP.Dlink>
2022-09-01 10:28:53 +0300

    Fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/dist/archimedes-frontend'));
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, "/dist/archimedes-frontend", "index.html"));
+app.use(express.static(__dirname + "/dist/archimedes-frontend"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/dist/archimedes-frontend", "index.html"));
 });
 app.listen(process.env.PORT || 8080);
