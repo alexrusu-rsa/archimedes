624b1f09312a54cffe8e9695880a441adae789e8
Calin Coroian <calincoroian@Calins-MacBook-Pro.local>
2023-10-17 12:44:16 +0300

    split users and rates to different pages

--- src/app/ng-modules/reporting/components/user-page/user-page.component.ts
+++ src/app/ng-modules/reporting/components/user-page/user-page.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { map, startWith, Subscription, switchMap } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 import { Project } from 'src/app/models/project';
-import { Rate } from 'src/app/models/rate';
 import { User } from 'src/app/models/user';
 import { ProjectService } from 'src/app/services/project-service/project.service';
-import { RateService } from 'src/app/services/rate-service/rate.service';
 import { UserManagePasswordService } from 'src/app/services/user-manage-password-service/user-manage-password.service';
 import { UserService } from 'src/app/services/user-service/user.service';
 import { DeleteConfirmationDialogComponent } from '../delete-confirmation-dialog/delete-confirmation-dialog.component';
 import { NewUserDialogComponent } from '../new-user-dialog/new-user-dialog.component';
-import { RateDialogComponent } from '../rate-dialog/rate-dialog.component';
 import { UserDialogComponent } from '../user-dialog/user-dialog.component';
 @Component({
   selector: 'app-user-page',
@@ -22,29 +19,16 @@ export class UserPageComponent implements OnInit, OnDestroy {
   deleteUserSubscription?: Subscription;
   users: User[] = [];
   search = '';
-  allRates: Rate[] = [];
   userResetPasswordSub?: Subscription;
-  allRatesSub?: Subscription;
-  deleteRateSub?: Subscription;
   projects?: Project[];
   fetchDataSubscription?: Subscription;
 
   test: string[] = ['ABC', 'def'];
 
-  displayedColumns: string[] = [
-    'projectId',
-    'employeeId',
-    'rate',
-    'rateType',
-    'employeeTimeCommitement',
-    'editButton',
-    'deleteButton',
-  ];
   projectsSub?: Subscription;
   constructor(
     private userService: UserService,
     private projectService: ProjectService,
-    private rateService: RateService,
     private userManagePasswordService: UserManagePasswordService,
     public dialog: MatDialog
   ) {}
@@ -65,14 +49,10 @@ export class UserPageComponent implements OnInit, OnDestroy {
         switchMap((users) => {
           this.allUsers = users;
           return this.projectService.getProjects();
-        }),
-        switchMap((projects) => {
-          this.projects = projects;
-          return this.rateService.getRates();
         })
       )
-      .subscribe((rates) => {
-        this.allRates = rates;
+      .subscribe((projects) => {
+        this.projects = projects;
       });
   }
 
@@ -93,7 +73,7 @@ export class UserPageComponent implements OnInit, OnDestroy {
   sendPasswordResetRequest(user: User) {
     this.userResetPasswordSub = this.userManagePasswordService
       .resetPasswordFor(user)
-      .subscribe((result) => {});
+      .subscribe();
   }
 
   checkSearch() {
@@ -113,45 +93,10 @@ export class UserPageComponent implements OnInit, OnDestroy {
     });
   }
 
-  addRate() {
-    const dialogRef = this.dialog.open(RateDialogComponent, {
-      panelClass: 'full-width-dialog',
-    });
-
-    dialogRef.afterClosed().subscribe((newRate: Rate) => {
-      this.fetchData();
-    });
-  }
-
-  editRate(rate: Rate) {
-    const dialogRef = this.dialog.open(RateDialogComponent, {
-      data: rate,
-      panelClass: 'full-width-dialog',
-    });
-    dialogRef.afterClosed().subscribe((updatedRate: Rate) => {
-      this.fetchData();
-    });
-  }
-
   editUser(user: User) {
     this.dialog.open(UserDialogComponent, {
       data: user,
       panelClass: 'full-width-dialog',
-    });
-  }
-
-  deleteRate(rate: Rate) {
-    const dialogRef = this.dialog.open(DeleteConfirmationDialogComponent, {
-      panelClass: 'delete-confirmation-dialog',
-    });
-    dialogRef.afterClosed().subscribe((result: boolean) => {
-      if (result) {
-        this.deleteRateSub = this.rateService
-          .deleteRate(rate.id!)
-          .subscribe((result) => {
-            this.fetchData();
-          });
-      }
     });
   }
 
