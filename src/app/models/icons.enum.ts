029b4f33a893656718cbf639b699b7277dea1657
Alex Rusu <rusualexrsa@gmail.com>
2024-05-01 12:17:23 +0300

    fix: cleanup admin-dashboard-page

--- src/app/models/icons.enum.ts
+++ src/app/models/icons.enum.ts
@@ -1,3 +1,10 @@
 export enum Icons {
   download = 'download',
+  event_note = 'event_note',
+  person = 'person',
+  people = 'people',
+  list_alt = 'list_alt',
+  assignment_turned_in = 'assignment_turned_in',
+  timelapse = 'timelapse',
+  money = 'money',
 }
