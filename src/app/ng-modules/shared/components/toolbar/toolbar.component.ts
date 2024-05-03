615875f1a8453b7d3379f0ef8f5c775ac8b1f313
Alex Rusu <rusualexrsa@gmail.com>
2024-05-03 21:04:49 +0300

    feat: add user access to the toolbar using userId

--- src/app/ng-modules/shared/components/toolbar/toolbar.component.ts
+++ src/app/ng-modules/shared/components/toolbar/toolbar.component.ts
@@ -1,23 +1,50 @@
+import { LocalStorageService } from './../../../../services/localstorage-service/localstorage.service';
 import {
-  ChangeDetectionStrategy,
   Component,
+  DestroyRef,
   EventEmitter,
   Input,
+  OnInit,
   Output,
+  inject,
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { of, switchMap } from 'rxjs';
 import { Icons } from 'src/app/models/icons.enum';
+import { User } from 'src/app/models/user';
+import { UserLoginService } from 'src/app/services/user-login-service/user-login.service';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ToolbarComponent {
-  @Input()
-  currentUserId?: string;
+export class ToolbarComponent implements OnInit {
+  readonly destroyRef = inject(DestroyRef);
   @Input()
   pageTitle?: string;
   @Output()
   toggleSidenav = new EventEmitter<void>();
   icons = Icons;
+  currentUser?: User | null;
+  currentUserId?: string | null;
+
+  constructor(
+    private service: UserLoginService,
+    private localStorageService: LocalStorageService
+  ) {}
+
+  ngOnInit(): void {
+    this.localStorageService.userIdValue
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        switchMap((currentUserId) => {
+          this.currentUserId = currentUserId;
+          if (currentUserId) return this.service.getUser(currentUserId);
+          else return of(null);
+        })
+      )
+      .subscribe((currentUser) => {
+        this.currentUser = currentUser;
+      });
+  }
 }
