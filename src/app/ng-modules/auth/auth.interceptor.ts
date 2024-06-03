3018a5481685a3bea3356065bbc6744cff98479a
alexrusu-rsa <rusualexrsa@gmail.com>
2024-06-03 12:46:39 +0300

    remove localstorage on token expire

--- src/app/ng-modules/auth/auth.interceptor.ts
+++ src/app/ng-modules/auth/auth.interceptor.ts
@@ -3,15 +3,21 @@ import {
   HttpInterceptor,
   HttpRequest,
   HttpHandler,
+  HttpErrorResponse,
 } from '@angular/common/http';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
 import { RoleCheckService } from 'src/app/services/rolecheck-service/rolecheck.service';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/services/localstorage-service/localstorage.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(
     private authService: AuthService,
-    private roleCheckService: RoleCheckService
+    private roleCheckService: RoleCheckService,
+    private router: Router,
+    private localStorageService: LocalStorageService
   ) {}
   intercept(req: HttpRequest<unknown>, next: HttpHandler) {
     const authToken = this.authService.getToken();
@@ -25,6 +31,20 @@ export class AuthInterceptor implements HttpInterceptor {
         userId: '' + userId,
       },
     });
-    return next.handle(req);
+
+    return next.handle(req).pipe(
+      tap(
+        () => {},
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status !== 401) {
+              return;
+            }
+            this.localStorageService.localStorageLogout();
+            this.router.navigate(['auth/login']);
+          }
+        }
+      )
+    );
   }
 }
