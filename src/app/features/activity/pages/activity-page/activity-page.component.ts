84bed9b1dddecf52a32a73c8d22dbe510909a1cb
Alex Rusu <rusualexrsa@gmail.com>
2024-06-14 11:47:07 +0300

    feat(activity): move activity entity to signal store for global access WIP

--- src/app/features/activity/pages/activity-page/activity-page.component.ts
+++ src/app/features/activity/pages/activity-page/activity-page.component.ts
@@ -1,13 +1,10 @@
-import { CommonModule, DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  DestroyRef,
   OnInit,
-  Signal,
   computed,
   inject,
-  signal,
 } from '@angular/core';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatCard, MatCardTitle, MatCardActions } from '@angular/material/card';
@@ -15,31 +12,19 @@ import { MatIcon } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { EntityItemComponent } from 'src/app/shared/components/entity-item/entity-item.component';
 import { EntityPageHeaderComponent } from 'src/app/shared/components/entity-page-header/entity-page-header.component';
-import { Activity } from 'src/app/shared/models/activity';
 import { Icons } from 'src/app/shared/models/icons.enum';
-import { ActivityService } from '../../services/activity-service/activity.service';
 import { LocalStorageService } from 'src/app/shared/services/localstorage-service/localstorage.service';
-import {
-  takeUntilDestroyed,
-  toObservable,
-  toSignal,
-} from '@angular/core/rxjs-interop';
-import { combineLatest, filter, map, of, switchMap, take } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map, switchMap } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectService } from 'src/app/features/project/services/project-service/project.service';
 import { MatDialog } from '@angular/material/dialog';
-import {
-  DeleteConfirmationModalComponent,
-  deleteConfirmationModalPreset,
-} from 'src/app/shared/components/delete-confirmation-modal/delete-confirmation-modal.component';
-import {
-  DuplicateActivityModalComponent,
-  duplicateActivityModalPreset,
-} from '../../components/duplicate-activity-modal/duplicate-activity-modal.component';
-import { ActivityModalComponent } from '../../components/activity-modal/activity-modal.component';
 import { OrderByPipe } from '../../pipes/order-by.pipe';
 import { WorkedTimePipe } from '../../pipes/worked-time.pipe';
-import { ActivityFilters } from '../../models/activity-filters.model';
+import { ActivityStore } from 'src/app/shared/store/activity.store';
+import { ActivityModalComponent } from '../../components/activity-modal/activity-modal.component';
+import { Activity } from 'src/app/shared/models/activity';
+import { ActivityService } from '../../services/activity-service/activity.service';
 
 @Component({
   selector: 'app-activity-page',
@@ -62,55 +47,21 @@ import { ActivityFilters } from '../../models/activity-filters.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActivityPageComponent implements OnInit {
-  private readonly destroyRef = inject(DestroyRef);
+  public readonly store = inject(ActivityStore);
   private readonly service = inject(ActivityService);
   private readonly projectService = inject(ProjectService);
   private readonly localStorage = inject(LocalStorageService);
-  private readonly datePipe = inject(DatePipe);
-  private readonly route = inject(ActivatedRoute);
   private readonly dialog = inject(MatDialog);
+  private readonly route = inject(ActivatedRoute);
   protected readonly icons = Icons;
-
-  protected filters = signal<ActivityFilters>({
-    date: new Date(),
-    project: null,
-  });
   protected projects = toSignal(
     this.projectService.getProjectsUser(this.localStorage.userId),
     { initialValue: [] }
   );
-  private rawActivities: Signal<Activity[]> = toSignal(
-    toObservable(this.filters).pipe(
-      switchMap((filters) =>
-        combineLatest([
-          this.service.getActivitiesByDateEmployeeId(
-            this.localStorage.userId,
-            this.datePipe.transform(filters?.date, 'dd/MM/yyyy')
-          ),
-          of(filters),
-        ])
-      ),
-      map(([activities, filters]) => {
-        // TODO handle 'other' project id in backend
-        if (filters?.project?.id === 'other')
-          return activities.filter((activity) => !activity?.project);
-        if (filters?.project?.id)
-          return activities.filter(
-            (activity) => activity?.project?.id === filters?.project?.id
-          );
-        return activities;
-      })
-    )
-  );
-  protected activities = computed(() => signal(this.rawActivities()));
+  protected activities = computed(() => this.store.activities());
   protected activityTypes = toSignal(this.service.getAllActivityTypes(), {
     initialValue: {},
   });
-
-  protected updateFilters(key, value) {
-    if (key === 'date') this.filters.set({ date: value, project: null });
-    else this.filters.set({ ...this.filters(), [key]: value });
-  }
 
   ngOnInit() {
     this.route.queryParams
@@ -119,26 +70,7 @@ export class ActivityPageComponent implements OnInit {
         filter((date) => !!date)
       )
       .subscribe((date) => {
-        this.updateFilters('date', new Date(date));
-      });
-  }
-
-  deleteAllActivity() {
-    this.dialog
-      .open(DeleteConfirmationModalComponent, deleteConfirmationModalPreset)
-      .afterClosed()
-      .pipe(
-        filter((deleteConfirmation) => deleteConfirmation === true),
-        switchMap((_) => {
-          return this.service.deleteAllActivitiesOfUserDay(
-            this.localStorage.userId,
-            this.datePipe.transform(this.filters()?.date, 'yyyy-MM-dd')
-          );
-        }),
-        take(1)
-      )
-      .subscribe((_) => {
-        this.activities().set([]);
+        this.updateFilter('date', new Date(date));
       });
   }
 
@@ -152,160 +84,174 @@ export class ActivityPageComponent implements OnInit {
         panelClass: 'full-width-dialog',
       })
       .afterClosed()
-      .pipe(
-        filter((activity: Activity) => !!activity),
-        switchMap((activity: Activity) => {
-          return this.service
-            .addActivity({
-              ...activity,
-              employeeId: this.localStorage.userId,
-              projectId: this.projects().find(
-                (project) => project.projectName === activity['projectName']
-              )?.id,
-              date: this.datePipe.transform(this.filters()?.date, 'dd/MM/yyyy'),
-            })
-            .pipe(takeUntilDestroyed(this.destroyRef));
-        }),
-        take(1)
-      )
+      .pipe(filter((activity: Activity) => !!activity))
       .subscribe((activity: Activity) => {
-        // TODO handle 'other' project id in backend
-        if (
-          activity?.project &&
-          this.filters()?.project?.id === activity?.project?.id
-        )
-          this.activities().update((activities) => [...activities, activity]);
-        if (
-          activity?.project === null &&
-          this.filters()?.project?.id === 'other'
-        )
-          this.activities().update((activities) => [...activities, activity]);
-        if (!this.filters()?.project?.id)
-          this.activities().update((activities) => [...activities, activity]);
+        this.store.addActivity(activity);
       });
   }
 
-  duplicateActivity(activity: Activity) {
-    this.dialog
-      .open(DuplicateActivityModalComponent, {
-        ...duplicateActivityModalPreset,
-        data: activity,
-      })
-      .afterClosed()
-      .pipe(
-        filter((activityDuplication) => !!activityDuplication),
-        switchMap((activityDuplication) =>
-          this.service.addDuplicates(activityDuplication)
-        ),
-        take(1)
-      )
-      .subscribe();
-  }
-
-  navigateToPreviousDate() {
-    this.filters.update((filters) => {
-      const nextDate = new Date(filters?.date);
-      nextDate.setDate(filters?.date.getDate() - 1);
-      return { date: nextDate };
-    });
-  }
-
-  navigateToNextDate() {
-    this.filters.update((filters) => {
-      const nextDate = new Date(filters?.date);
-      nextDate.setDate(filters?.date.getDate() + 1);
-      return { date: nextDate };
-    });
-  }
-
-  editActivity(activity: Activity) {
-    const {
-      id,
-      employeeId,
-      date,
-      projectId,
-      project,
-      workedTime,
-      ...activityWithoutUnecessary
-    } = activity;
-    this.dialog
-      .open(ActivityModalComponent, {
-        data: {
-          activity: {
-            ...activityWithoutUnecessary,
-            projectName: activity?.project?.projectName
-              ? activity?.project?.projectName
-              : 'other',
-          },
-          activityProjects: this.projects(),
-          activityTypes: Object.values(this.activityTypes()),
-        },
-        panelClass: 'full-width-dialog',
-      })
-      .afterClosed()
-      .pipe(
-        filter((activity: Activity) => !!activity),
-        switchMap((updatedActivity: Activity) => {
-          const { projectName, ...updatedActivityWithoutProjectName } =
-            updatedActivity;
-          return this.service
-            .updateActivity({
-              ...updatedActivityWithoutProjectName,
-              id: activity?.id,
-              employeeId: activity?.employeeId,
-              date: activity?.date,
-              projectId:
-                this.projects()?.find(
-                  (project) =>
-                    project?.projectName === updatedActivity?.projectName
-                )?.id || null,
-            })
-            .pipe(takeUntilDestroyed(this.destroyRef));
-        }),
-        take(1)
-      )
-      .subscribe((updatedActivity: Activity) => {
-        this.activities().update((activities) => {
-          // TODO handle 'other' project id in backend
-          if (this.filters().project?.id)
-            return activities
-              .map((activity) => {
-                if (updatedActivity?.id === activity?.id)
-                  return updatedActivity;
-                return activity;
-              })
-              .filter(
-                (activity) =>
-                  activity?.project?.id === this.filters().project.id ||
-                  (this.filters().project.id === 'other' && !activity.project)
-              );
-
-          // base case
-          return activities.map((activity) => {
-            if (updatedActivity?.id === activity?.id) return updatedActivity;
-            return activity;
-          });
+  protected updateFilter(key: string, value) {
+    switch (key) {
+      case 'date':
+        this.store.updateFilter({
+          date: value,
+          project: null,
         });
-      });
+        break;
+      case 'project':
+        this.store.updateFilter({
+          project: value,
+          date: this.store.filter()?.date,
+        });
+        break;
+      default:
+        break;
+    }
   }
 
-  deleteActivity(id: string) {
-    this.dialog
-      .open(DeleteConfirmationModalComponent, deleteConfirmationModalPreset)
-      .afterClosed()
-      .pipe(
-        filter((deleteConfirmation) => deleteConfirmation === true),
-        switchMap((_) => {
-          return this.service
-            .deleteActivity(id)
-            .pipe(takeUntilDestroyed(this.destroyRef));
-        }),
-        take(1)
-      )
-      .subscribe((_) => {
-        this.activities().update((activities) =>
-          activities.filter((activity) => activity.id !== id)
-        );
-      });
-  }
+  // deleteAllActivity() {
+  //   this.dialog
+  //     .open(DeleteConfirmationModalComponent, deleteConfirmationModalPreset)
+  //     .afterClosed()
+  //     .pipe(
+  //       filter((deleteConfirmation) => deleteConfirmation === true),
+  //       switchMap((_) => {
+  //         return this.service.deleteAllActivitiesOfUserDay(
+  //           this.localStorage.userId,
+  //           this.datePipe.transform(this.store.filter()?.date, 'yyyy-MM-dd')
+  //         );
+  //       }),
+  //       take(1)
+  //     )
+  //     .subscribe((_) => {
+  //       // TODO update state in store
+  //       // this.activities().set([]);
+  //     });
+  // }
+
+  // duplicateActivity(activity: Activity) {
+  //   this.dialog
+  //     .open(DuplicateActivityModalComponent, {
+  //       ...duplicateActivityModalPreset,
+  //       data: activity,
+  //     })
+  //     .afterClosed()
+  //     .pipe(
+  //       filter((activityDuplication) => !!activityDuplication),
+  //       switchMap((activityDuplication) =>
+  //         this.service.addDuplicates(activityDuplication)
+  //       ),
+  //       take(1)
+  //     )
+  //     .subscribe();
+  // }
+
+  // navigateToPreviousDate() {
+  //   this.updateFilter('date'.)
+  //   this.filter.update((filters) => {
+  //     const nextDate = new Date(filters?.date);
+  //     nextDate.setDate(filters?.date.getDate() - 1);
+  //     return { date: nextDate };
+  //   });
+  // }
+
+  // navigateToNextDate() {
+  //   this.filters.update((filters) => {
+  //     const nextDate = new Date(filters?.date);
+  //     nextDate.setDate(filters?.date.getDate() + 1);
+  //     return { date: nextDate };
+  //   });
+  // }
+
+  // editActivity(activity: Activity) {
+  //   const {
+  //     id,
+  //     employeeId,
+  //     date,
+  //     projectId,
+  //     project,
+  //     workedTime,
+  //     ...activityWithoutUnecessary
+  //   } = activity;
+  //   this.dialog
+  //     .open(ActivityModalComponent, {
+  //       data: {
+  //         activity: {
+  //           ...activityWithoutUnecessary,
+  //           projectName: activity?.project?.projectName
+  //             ? activity?.project?.projectName
+  //             : 'other',
+  //         },
+  //         activityProjects: this.projects(),
+  //         activityTypes: Object.values(this.activityTypes()),
+  //       },
+  //       panelClass: 'full-width-dialog',
+  //     })
+  //     .afterClosed()
+  //     .pipe(
+  //       filter((activity: Activity) => !!activity),
+  //       switchMap((updatedActivity: Activity) => {
+  //         const { projectName, ...updatedActivityWithoutProjectName } =
+  //           updatedActivity;
+  //         return this.service
+  //           .updateActivity({
+  //             ...updatedActivityWithoutProjectName,
+  //             id: activity?.id,
+  //             employeeId: activity?.employeeId,
+  //             date: activity?.date,
+  //             projectId:
+  //               this.projects()?.find(
+  //                 (project) =>
+  //                   project?.projectName === updatedActivity?.projectName
+  //               )?.id || null,
+  //           })
+  //           .pipe(takeUntilDestroyed(this.destroyRef));
+  //       }),
+  //       take(1)
+  //     )
+  //     .subscribe((updatedActivity: Activity) => {
+  //       // TODO update state in store
+  //       // this.activities().update((activities) => {
+  //       //   // TODO handle 'other' project id in backend
+  //       //   if (this.filters().project?.id)
+  //       //     return activities
+  //       //       .map((activity) => {
+  //       //         if (updatedActivity?.id === activity?.id)
+  //       //           return updatedActivity;
+  //       //         return activity;
+  //       //       })
+  //       //       .filter(
+  //       //         (activity) =>
+  //       //           activity?.project?.id === this.filters().project.id ||
+  //       //           (this.filters().project.id === 'other' && !activity.project)
+  //       //       );
+  //       //   // base case
+  //       //   return activities.map((activity) => {
+  //       //     if (updatedActivity?.id === activity?.id) return updatedActivity;
+  //       //     return activity;
+  //       //   });
+  //       // });
+  //     });
+  // }
+
+  // deleteActivity(id: string) {
+  //   this.dialog
+  //     .open(DeleteConfirmationModalComponent, deleteConfirmationModalPreset)
+  //     .afterClosed()
+  //     .pipe(
+  //       filter((deleteConfirmation) => deleteConfirmation === true),
+  //       switchMap((_) => {
+  //         return this.service
+  //           .deleteActivity(id)
+  //           .pipe(takeUntilDestroyed(this.destroyRef));
+  //       }),
+  //       take(1)
+  //     )
+  //     .subscribe((_) => {
+  //       // TODO update state in store
+  //       // this.activities().update((activities) =>
+  //       //   activities.filter((activity) => activity.id !== id)
+  //       // );
+  //     });
+  // }
 }
---
+++ src/app/shared/store/activity.store.ts
@@ -0,0 +1,126 @@
+import { inject } from '@angular/core';
+import {
+  patchState,
+  signalStore,
+  withHooks,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { ActivityService } from 'src/app/features/activity/services/activity-service/activity.service';
+import { Activity } from 'src/app/shared/models/activity';
+import { Project } from 'src/app/shared/models/project';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { debounceTime, distinctUntilChanged, pipe, switchMap, tap } from 'rxjs';
+import { LocalStorageService } from '../services/localstorage-service/localstorage.service';
+import { ActivityFilter } from 'src/app/features/activity/models/activity-filter.model';
+import { DatePipe } from '@angular/common';
+import { tapResponse } from '@ngrx/operators';
+import { ProjectService } from 'src/app/features/project/services/project-service/project.service';
+
+type ActivityState = {
+  activities: Activity[];
+  isLoading: boolean;
+  filter: { project?: Project; date?: Date };
+};
+
+const initialState: ActivityState = {
+  activities: [],
+  isLoading: false,
+  filter: { project: null, date: new Date() },
+};
+
+export const ActivityStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+  withMethods(
+    (
+      store,
+      activityService = inject(ActivityService),
+      localStorage = inject(LocalStorageService),
+      projectService = inject(ProjectService),
+      datePipe = inject(DatePipe)
+    ) => ({
+      updateFilter(filter: ActivityFilter) {
+        patchState(store, { filter });
+      },
+      loadActivitiesByFilter: rxMethod<ActivityFilter>(
+        pipe(
+          debounceTime(300),
+          distinctUntilChanged(),
+          tap(() => patchState(store, { isLoading: true })),
+          switchMap((filter: ActivityFilter) =>
+            activityService
+              .getActivitiesByDateEmployeeId(
+                localStorage.userId,
+                datePipe.transform(filter?.date, 'dd/MM/yyyy')
+              )
+              .pipe(
+                tapResponse({
+                  next: (activities: Activity[]) =>
+                    patchState(store, {
+                      activities: activities.filter((activity: Activity) => {
+                        if (!filter?.project) return !!activity;
+
+                        if (filter?.project?.id === 'other')
+                          return !activity?.project;
+                        else
+                          return activity?.project?.id === filter?.project?.id;
+                      }),
+                    }),
+                  // eslint-disable-next-line no-console
+                  error: (error) => console.error(error),
+                  finalize: () => patchState(store, { isLoading: false }),
+                })
+              )
+          )
+        )
+      ),
+      addActivity: rxMethod<Activity>(
+        pipe(
+          debounceTime(300),
+          switchMap((activity) =>
+            activityService
+              .addActivity({
+                ...activity,
+                employeeId: localStorage?.userId,
+                projectId: activity?.project?.id,
+                date: datePipe.transform(store.filter()?.date, 'dd/MM/yyyy'),
+              })
+              .pipe(
+                tapResponse({
+                  next: (activity: Activity) => {
+                    if (!store.filter()?.project)
+                      patchState(store, {
+                        activities: [...store.activities(), activity],
+                      });
+                    else {
+                      if (
+                        store.filter()?.project?.id === 'other' &&
+                        !activity?.project
+                      )
+                        patchState(store, {
+                          activities: [...store.activities(), activity],
+                        });
+
+                      if (store.filter()?.project?.id === activity?.project?.id)
+                        patchState(store, {
+                          activities: [...store.activities(), activity],
+                        });
+                    }
+                  },
+                  // eslint-disable-next-line no-console
+                  error: (error) => console.error(error),
+                  finalize: () => patchState(store, { isLoading: false }),
+                })
+              )
+          )
+        )
+      ),
+    })
+  ),
+  withHooks({
+    onInit({ loadActivitiesByFilter, filter }) {
+      loadActivitiesByFilter(filter);
+    },
+  })
+);
