57c01bc3c2f193ed5e01513a7445d8fc3b2ede3c
calinc2010 <33434438+calinc2010@users.noreply.github.com>
2024-06-23 12:34:27 +0300

    wip

---
+++ src/app/features/rate/components/rate-modal/rate-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rate-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './rate-modal.component.html',
+  styleUrl: './rate-modal.component.sass'
+})
+export class RateModalComponent {
+
+}
