79e6f43356c001478cf68dfce547cdd1afcdf37b
Calin Coroian <calincoroian@Calins-MacBook-Pro.local>
2024-07-05 13:02:44 +0300

    fix comments

--- src/app/shared/components/initials-icon/initials-icon.component.ts
+++ src/app/shared/components/initials-icon/initials-icon.component.ts
@@ -2,18 +2,16 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { User } from '../../models/user';
+import { MatCardSubtitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-initials-icon',
   standalone: true,
-  imports: [CommonModule, MatIcon],
+  imports: [CommonModule, MatIcon, MatCardSubtitle],
   templateUrl: './initials-icon.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styles: `
     @use 'src/styles/variables.sass' as variables
-
-    .custom-icon
-      color: variables.$user-navigation-header
   `,
 })
 export class InitialsIconComponent {
